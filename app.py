7235361727422666d0670d0eb93c9bc5467ac0d6
Samyak Jain <samjain06@gmail.com>
2023-06-30 20:30:11 -0500

    added database

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -11,38 +12,25 @@ def index():  # put application's code here
 def html_page(page_name=None):
     return render_template(page_name)
 
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
-#
-# @app.route('/work.html')
-# def work():
-#     return render_template('work.html')
-#
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
-
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        file = database.write(f'\n{email},{subject},{message}')
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        return redirect('thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to database'
     else:
         return 'Something went wrong'
-
 
 if __name__ == '__main__':
     app.run()
